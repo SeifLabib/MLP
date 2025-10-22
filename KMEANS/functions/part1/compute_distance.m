0f31965cf23bc23f3f40b3c9191f831cb9ad30fa
Seif Labib <seiflabib2001@gmail.com>
2025-10-22 05:00:37 +0200

    Lab 2 KMeans start

---
+++ KMEANS/functions/part1/compute_distance.m
@@ -0,0 +1,17 @@
+function [d] =  compute_distance(x_1, x_2, type)
+%COMPUTE_DISTANCE Computes the distance between two datapoints (as column vectors)
+%   depending on the choosen distance type={'L1','L2','LInf'}
+%
+%   input -----------------------------------------------------------------
+%   
+%       o x_1   : (N x 1),  N-dimensional datapoint
+%       o x_2   : (N x 1),  N-dimensional datapoint
+%       o type  : (string), type of distance {'L1','L2','LInf'}
+%
+%   output ----------------------------------------------------------------
+%
+%       o d      : distance between x_1 and x_2 depending on distance
+%                  type {'L1','L2','LInf'}
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+end
